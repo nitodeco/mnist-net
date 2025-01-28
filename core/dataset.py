1f12985d746211cf936c07da8353460afdec9f34
nitodeco <n.moehn@outlook.com>
2025-01-28 23:47:30 +0100

    add dataset augmentation

--- core/dataset.py
+++ core/dataset.py
@@ -1,10 +1,18 @@
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.RandomRotation(8),
+        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+    ]
+)
 
 training_data = datasets.MNIST(
     root="data",
     train=True,
-    transform=ToTensor(),
+    transform=transform,
     download=True,
 )
-test_data = datasets.MNIST(root="data", train=False, transform=ToTensor())
+test_data = datasets.MNIST(root="data", train=False, transform=transform)
